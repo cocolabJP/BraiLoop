8d26ee45b2923594cf2457faa05bf378a131b74b
yuto-matsuda <yutochin.dev@gmail.com>
2026-01-05 23:16:17 +0900

    Modify API and Docs

--- api/app/filters/tactile_paving.py
+++ api/app/filters/tactile_paving.py
@@ -9,8 +9,8 @@ class TactilePavingFilter(str, Enum):
 def apply_tactile_paving_filter(query, value: TactilePavingFilter):
     FILTER_MAP = {
         TactilePavingFilter.all: lambda q: q.not_.is_('classes', None),
-        TactilePavingFilter.warning: lambda q: q.contains('classes', [0]),
-        TactilePavingFilter.guiding: lambda q: q.contains('classes', [1]),
+        TactilePavingFilter.warning: lambda q: q.contains('classes', ['0']),
+        TactilePavingFilter.guiding: lambda q: q.contains('classes', ['1']),
         TactilePavingFilter.none: lambda q: q.is_('classes', None),
     }
     return FILTER_MAP[value](query)
--- webapp/src/pages/ApiDoc.tsx
+++ webapp/src/pages/ApiDoc.tsx
@@ -56,7 +56,7 @@ export default function ApiDoc() {
               <Table contents={pavementsQueryParamsTable} centralizeIndices={[0, 1]} boldIndices={[0]} />
               <p>リクエスト例:</p>
               {/* <Code block>https://xxx/pavements?tactile_paving=all&start_at=2026-01-01T06:30:00Z</Code> */}
-              <Code block>https://xxx/pavements?tactile_paving=all</Code>
+              <Code block>https://api.brailoop.cocolab.jp/v1/pavements?tactile_paving=all</Code>
               <div role="alert" className="alert alert-warning my-4">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
